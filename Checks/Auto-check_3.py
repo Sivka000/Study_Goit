a82f898a6742d14e3689c54909e1a5043afa2155
Sivka000 <stepchynv@gmail.com>
2025-05-25 12:21:08 +0300

    T.2

--- Checks/Auto-check_3.py
+++ Checks/Auto-check_3.py
@@ -11,3 +11,20 @@ converted_date = string_to_date(date_string)
 print(converted_date)
 date_string = date_to_string(converted_date)
 print(date_string)
+
+def prepare_user_list(users):
+    prepared_users = []
+    for user in users:
+        birthday = string_to_date(user["birthday"])
+        prepared_users.append({"name": user["name"], "birthday": birthday})
+    return prepared_users
+
+users = [
+    {"name": "Bill Gates", "birthday": "1955.3.25"},
+    {"name": "Steve Jobs", "birthday": "1955.3.21"},
+    {"name": "Jinny Lee", "birthday": "1956.3.22"},
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"}
+]
+prepared_users = prepare_user_list(users)
+print(prepared_users)
